3b9d7519a3a1abce7c1f969b22f310a479fc4a36
datow <datow.official@gmail.com>
2020-07-09 12:17:48 -0600

    final emoji update

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
 	name: 'help',
 	description: 'help',
 	execute(message, args) {
-		message.react(':white_check_mark:');
+		message.react('✅');
 		const exampleEmbed = new Discord.MessageEmbed()
 			.setColor('#922b3e')
 			.setTitle('77 Bot Help!')
