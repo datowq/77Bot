cdd99cdce5eee40db5229438654ad2bf879de53b
datow <datow.official@gmail.com>
2020-07-09 12:11:41 -0600

    more emote work

--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,7 @@ module.exports = {
 	name: 'help',
 	description: 'help',
 	execute(message, args) {
+		message.react('730247445396848680');
 		const exampleEmbed = new Discord.MessageEmbed()
 			.setColor('#922b3e')
 			.setTitle('77 Bot Help!')
@@ -15,6 +16,5 @@ module.exports = {
 			.setDescription('```diff\n->  `server```spits out some server info\n```diff\n->  `user {username}```spits out some user info\n```diff\n->  `createchannel {channelname}```creates a channel of your liking\n```diff\n->  `kick {username}```bye bye :)\n```diff\n->  `ban {username}```bye bye big boy\n```diff\n->  `kyaru```sends a random image of the best catgirl known to man```diff\n->  `arknights```sends a random image from the best game known to man\n')
 
 		message.author.send(exampleEmbed);
-		message.react('730247445396848680');
 	},
 };
