765e46e183a80e75dfee9f3b734d21390ec12e18
datow <datow.official@gmail.com>
2020-07-09 20:46:34 -0600

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -8,14 +8,19 @@ module.exports = {
 		if (user) {
 			const member = message.guild.member(user);
 			const roles = member.roles.cache;
+			const roles = user.roles.cache
+            .filter(r => r.id !== user.id)
+            .map(r => r.name).join(", ") || 'none';
 			message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}\nRoles: ${member.roles}`);
 	
 		}
 		else {
 			const user = message.author;
 			const member = message.guild.member(user);
-			const roles = member.roles.cache;
-			return message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}\nRoles: ${member.roles}`);
+			const roles = user.roles.cache
+            .filter(r => r.id !== user.id)
+            .map(r => r.name).join(", ") || 'none';
+			return message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}\nRoles: ${roles}`);
 		}
 	},
 };
