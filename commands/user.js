8b8db53d53d3c377b75813fd72b801156f42e85e
datow <datow.official@gmail.com>
2020-07-09 20:05:38 -0600

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -7,13 +7,13 @@ module.exports = {
 		const user = message.mentions.users.first();
 		if (user) {
 			const member = message.guild.member(user);
-			message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}`);
+			message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}\nRoles: ${member.roles}`);
 	
 		}
 		else {
 			const user = message.author;
 			const member = message.guild.member(user);
-			return message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}`);
+			return message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}\nRoles: ${member.roles}`);
 		}
 	},
 };
