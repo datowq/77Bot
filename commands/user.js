92378f890457ed64b503fbd57fce8985a6ff0b40
datow <datow.official@gmail.com>
2020-07-21 19:33:03 -0600

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -5,16 +5,26 @@ module.exports = {
 	description: 'user',
 	run: async (client, message, args) => {
 
-		let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+		const user = message.mentions.users.first();
+		const member = message.guild.member(user);
+		let role = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
 
 
-		const roles = member.roles.cache
+		const roles = role.roles.cache
             .filter(r => r.id !== message.guild.id)
 			.map(r => r.name).join(", ") || 'none';
 			
 
-		message.channel.send(`Tag: ${member.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.createdAt}\nJoined On: ${member.joinedAt}\nRoles: ${roles}`);
+		if (user) {
+			const member = message.guild.member(user);
+			message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}\nRoles: ${roles}`);
 	
+		}
+		else {
+			const user = message.author;
+			const member = message.guild.member(user);
+			return message.channel.send(`Tag: ${member.user.tag}\nNickname: ${member.displayName}\nID: ${member.id}\nAccount Created: ${member.user.createdAt}\nJoined On: ${member.joinedAt}\nRoles: ${roles}`);
+		}
 
 		
 	},
