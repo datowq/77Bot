fdded0c94b58ccc2ee4e72dae17a85ab09b05a00
datow <datow.official@gmail.com>
2020-07-22 00:42:10 -0600

    Update createchannel.js

--- commands/createchannel.js
+++ commands/createchannel.js
@@ -7,7 +7,7 @@ module.exports = {
 
 		if (message.member.hasPermission('MANAGE_CHANNELS')) {
 
-			const type = message.content.slice(15, name.indexOf(' '));
+			const type = message.content.slice(15, message.conent.indexOf(' '));
 			const name = type.substr(name.indexOf(' ')+1);
 			
 			if(type === "voice" || type === "Voice" || type === "v" || type === "V") {
