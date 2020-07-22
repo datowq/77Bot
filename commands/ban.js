5d929aa62643f01019f38cc68e1fc46e7f727063
datow <datow.official@gmail.com>
2020-07-21 22:55:48 -0600

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,28 +5,24 @@ module.exports = {
 	name: 'ban',
 	description: 'ban',
 	run: async (client, message, args) => {
+		 
 		let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
 
-			if (member) {
+			if (message.member.hasPermission('BAN_MEMBERS')) {
 
+					member.ban('Bye').then(() => {
+						return message.reply(`bye bye ${user.tag}`);
+					})
+					.catch(err => {
+						return message.reply('What?!');
 
-				if (message.member.hasPermission('BAN_MEMBERS')) {
-
-						member.ban('Bye').then(() => {
-							return message.reply(`bye bye @${user.tag}`);
-						})
-						.catch(err => {
-							return message.reply('What?!');
-
-							console.error(err);
-						});
-				}
-				else {
-					return message.reply("you don't have perms lmao");
-				}
+						console.error(err);
+					});
 			}
-		else {
-			return message.reply("you didn't mention the user to ban!");
-		}
+
+			else {
+				return message.reply("you don't have perms lmao");
+			}
+		
 	},
 };
