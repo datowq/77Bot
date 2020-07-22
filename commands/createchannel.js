6af00be8b058f72c92e9b7c1a19cbcfa5119f584
datow <datow.official@gmail.com>
2020-07-22 00:24:00 -0600

    Update createchannel.js

--- commands/createchannel.js
+++ commands/createchannel.js
@@ -4,12 +4,48 @@ module.exports = {
 	name: 'createchannel',
 	description: 'createchannel',
 	execute(message, args) {
+
 		if (message.member.hasPermission('MANAGE_CHANNELS')) {
+
 			const channelname = message.content.slice(15);
-			message.guild.channels.create(`${channelname}`);
+			
+			if(channelname === "voice" || channelname === "Voice" || channelname === "v" || channelname === "V") {
+
+				// Create a new channel with permission overwrites
+				guild.channels.create('new voice', {
+					type: 'voice',
+					permissionOverwrites: [
+					{
+						id: message.author.id,
+						deny: ['VIEW_CHANNEL'],
+					},
+					],
+				})
+
+			}
+
+			if(channelname === "text" || channelname === "Text" || channelname === "t" || channelname === "T") {
+				
+				// Create a new channel with permission overwrites
+				guild.channels.create('new text', {
+					type: 'text',
+					permissionOverwrites: [
+					{
+						id: message.author.id,
+						deny: ['VIEW_CHANNEL'],
+					},
+					],
+				})
+
+			}
+
 		}
+
 		else {
+
 			return message.reply("you don't have perms lmao");
+
 		}
+
 	},
 };
