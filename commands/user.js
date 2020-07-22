6acccef4a0734f59dcdaebbab676f9f7ef390bc5
datow <datow.official@gmail.com>
2020-07-21 19:28:17 -0600

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -1,14 +1,12 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
 module.exports = {
 	name: 'user',
 	description: 'user',
 	run: async (client, message, args) => {
 
-		async (client, message, args) => {
-			let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
-		}
+		let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+
 
 		const roles = member.roles.cache
             .filter(r => r.id !== message.guild.id)
