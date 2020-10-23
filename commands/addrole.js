a1f4ee07d0a9a1b213ad6a508ddffc1b77d75138
datow <datow.official@gmail.com>
2020-10-23 13:12:19 -0600

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -9,12 +9,11 @@ module.exports = {
 		
             
             let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
-
-            let myRole = message.guild.roles.find(role => role.name === "Senrole");
-            let myRole1 = message.guild.roles.find(role => role.name === "Owner");
+            
 
                 member.addRole(myRole).then(() => {
                     message.member.roles.add("721960072976138240");
+                    message.member.roles.add("737243450277363764");
                     member.addRole(myRole1);
                     return message.channel.send(`added ${member.user.tag}`);
                     
